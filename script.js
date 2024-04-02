93c09265a3fce64b9e5660c157d36a0739b77612
zubowaantonina <zubowaantonina@yandex.ru>
2024-04-02 17:04:07 +0300

    изменила многомерный массив

--- script.js
+++ script.js
@@ -2,30 +2,38 @@
 let lang = prompt("ru/en");
 console.log(lang);
 if (lang == "ru") {
-    console.log("пн", "вт", "ср", "чт", "пт", "сб", "вс");
+  console.log("пн", "вт", "ср", "чт", "пт", "сб", "вс");
 } else if (lang == "en") {
-    console.log("mn", "ts", "wd", "th", "fr", "st", "sn");
+  console.log("Mo", "Tu", "We", "Th", "Fr", "Sa", "Su");
 }
 switch (lang) {
-    case "ru":
-        console.log("пн", "вт", "ср", "чт", "пт", "сб", "вс");
-        break;
-    case "en":
-        console.log("mn", "ts", "wd", "th", "fr", "st", "sn");
-        break;
+  case "ru":
+    console.log("пн", "вт", "ср", "чт", "пт", "сб", "вс");
+    break;
+  case "en":
+    console.log("Mo", "Tu", "We", "Th", "Fr", "Sa", "Su");
+    break;
 }
 const Arr = {
-    ru: ["пн", "вт", "ср", "чт", "пт", "сб", "вс"],
-    en: ["mn", "ts", "wd", "th", "fr", "st", "sn"],
+  ru: ["пн", "вт", "ср", "чт", "пт", "сб", "вс"],
+  en: ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"],
 };
 console.log(Arr[lang]);
+
+const daysOfWeek = [
+  ["ru", "пн", "вт", "ср", "чт", "пт", "сб", "вс"],
+  ["en", "Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"],
+];
+daysOfWeek[0][0] == lang
+  ? console.log(daysOfWeek[0].slice(1))
+  : console.log(daysOfWeek[1].slice(1));
 
 let namePerson = prompt("имя");
 namePerson = namePerson.toLowerCase();
 console.log(
-    namePerson === "артем"
-        ? "Директор"
-        : namePerson === "александр"
-            ? "Преподователь"
-            : "Студент"
+  namePerson === "артем"
+    ? "Директор"
+    : namePerson === "александр"
+    ? "Преподователь"
+    : "Студент"
 );
