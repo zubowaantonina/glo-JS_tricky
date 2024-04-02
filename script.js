306c271aa0d159bdbb71f2500b2b69fceec43505
zubowaantonina <zubowaantonina@yandex.ru>
2024-04-02 23:05:29 +0300

    lesson04

---
+++ script.js
@@ -0,0 +1,17 @@
+"use strict";
+let str ="   В JavaScript нет защиты от дурака. Мы считаем, что все, кто работают над кодом — профессионалы и доверяем им.   ";
+console.log(str);
+const myFunc = (str) => {
+    if (typeof str !== "string") {
+        console.log(" Не строка");
+    } else {
+        str = str.trim(); //убираем пробелы
+        console.log(str);
+        let cutStr = str.slice(0, 30);
+        if (str.length > 30) {
+            cutStr = cutStr += "...";
+        }
+        console.log(cutStr);
+    }
+}
+myFunc(str)
